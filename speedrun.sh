262c5560cec3853cca774fb8f551ffca33c016d2
Paul Tindall <paul.tindall@rxfoundry.com>
2026-01-08 18:59:16 -0600

    chore: another attempt to fix segfault / ld loading issues.

--- speedrun.sh
+++ speedrun.sh
@@ -15,6 +15,9 @@ export OMP_NUM_THREADS=1
 export NANOCHAT_BASE_DIR="$HOME/.cache/nanochat"
 mkdir -p $NANOCHAT_BASE_DIR
 
+# Workaround for "Inconsistency detected by ld.so" error, often caused by LD_PRELOAD (e.g. tcmalloc)
+unset LD_PRELOAD
+
 # -----------------------------------------------------------------------------
 # Python venv setup with uv
 
