5b61a477afe571ebe3256ee8de1ae55547cf6898
Paul Tindall <paul.tindall@rxfoundry.com>
2026-01-26 11:39:25 -0600

    chore: fix dir

--- runs/beemo_5080_run.sh
+++ runs/beemo_5080_run.sh
@@ -58,6 +58,7 @@ python -m nanochat.report reset
 # each shard is ~100MB of text (compressed), so this is about ~800MB of data on disk
 python -m nanochat.dataset_rxf -n 370 -w 4
 rm -f ~/backups/nanochat/base_data/*.parquet
+mkdir ~/.cache/nanochat/base_data
 cp ~/.cache/nanochat/base_data/* ~/backups/nanochat/base_data
 
 # Immediately also kick off downloading more shards in the background while tokenizer trains
