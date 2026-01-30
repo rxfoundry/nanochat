ecb9b9076aa0763d9e156d059c2ad21d8c33a04d
Paul Tindall <paul.tindall@rxfoundry.com>
2026-01-30 12:46:46 -0600

    19 was super slow.  bumping down to 18.

--- runs/beemo_5080_wsl_run.sh
+++ runs/beemo_5080_wsl_run.sh
@@ -88,7 +88,7 @@ python -m scripts.tok_eval
 
 # pretrain the d18 model
 python -m scripts.base_train_rxf \
-    --depth=19 \
+    --depth=18 \
     --target-param-data-ratio=20 \
     --window-pattern=L \
     --device-batch-size=1 \
@@ -113,7 +113,7 @@ curl -L -o $NANOCHAT_BASE_DIR/identity_conversations.jsonl https://karpathy-publ
 
 # run midtraining and eval the model
 python -m scripts.mid_train \
-    --depth=19 \
+    --depth=18 \
     --target-param-data-ratio=20 \
     --window-pattern=L \
     --device-batch-size=1 \
